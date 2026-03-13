bf255536d5cadecc9cc4a334a1b71c7dca647088
Eigenverft <eigenverft@outlook.com>
2026-03-13 05:21:42 +0100

    update bootstrapper

---
+++ iwr/bootstrapper.command.ps1
@@ -0,0 +1,22 @@
+<#
+Generic PSGallery bootstrapper for installing/importing configurable modules,
+then opening a new Windows PowerShell console and invoking a configurable command.
+
+Configure:
+
+$i = modules to install and import
+$c = command to invoke in the new console
+
+Current defaults:
+
+$i='PowerShellGet','PackageManagement','Eigenverft.Manifested.Sandbox'
+$c=''
+
+This is the more generic variant of iwr/bootstrapper.ps1.
+The published bootstrapper one-liner stays unchanged.
+
+$c='';$i='PowerShellGet','PackageManagement','Eigenverft.Manifested.Sandbox';iwr -useb https://raw.githubusercontent.com/eigenverft/Eigenverft.Manifested.Sandbox/refs/heads/main/iwr/bootstrapper.generic.ps1 | iex
+
+#>
+
+$c='';$i='PowerShellGet','PackageManagement','Eigenverft.Manifested.Sandbox';$s='CurrentUser';$g='PSGallery';$u='https://www.powershellgallery.com/api/v2';if($PSVersionTable.PSVersion.Major-ne5){return};try{Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted -Force}catch{};try{[Net.ServicePointManager]::SecurityProtocol=[Net.SecurityProtocolType]::Tls12}catch{};[System.Net.WebRequest]::DefaultWebProxy=[System.Net.WebRequest]::GetSystemWebProxy();if([System.Net.WebRequest]::DefaultWebProxy){[System.Net.WebRequest]::DefaultWebProxy.Credentials=[System.Net.CredentialCache]::DefaultNetworkCredentials};$v=[version]'2.8.5.201';Install-PackageProvider NuGet -MinimumVersion $v -Scope $s -Force -ForceBootstrap|Out-Null;try{Set-PSRepository $g -InstallationPolicy Trusted -ea Stop}catch{Register-PSRepository $g -SourceLocation $u -ScriptSourceLocation $u -InstallationPolicy Trusted -ea Stop};Find-Module $i -Repository $g|select Name,Version|?{-not(Get-Module -ListAvailable $_.Name|sort Version -desc|select -f 1|? Version -eq $_.Version)}|%{Install-Module $_.Name -RequiredVersion $_.Version -Repository $g -Scope $s -Force -AllowClobber;try{Remove-Module $_.Name -ea 0}catch{};Import-Module $_.Name -MinimumVersion $_.Version -Force};Start-Process cmd "/c start `"`" powershell -NoExit -Command `"$c;`"";exit
